2fb696a3d69b916cb20e6edcab0ff990ae13a1e7
lutcraft <739750084@qq.com>
2023-09-17 19:23:37 +0800

    聚焦窗口

--- renderer.js
+++ renderer.js
@@ -4,14 +4,38 @@ information.innerText = `本应用正在使用 Chrome (v${versions.chrome()}), N
 
 
 const newWindowBtn = document.getElementById('new-window')
+const focusModalBtn = document.getElementById('focus-on-modal-window')
 
 newWindowBtn.addEventListener('click', (event) => {
     const modalPath = versions.path().join('file://', versions.__dirname(), './pages/windows/modal.html')
-    const childWindow = window.open(modalPath)
-    // let win = new BrowserWindow({ width: 400, height: 320 })
-    // win.on('close', () => { win = null })
-    // win.loadURL(modalPath)
-    // win.show()
+    const childWindow = window.open(modalPath, '_blank', 'top=200,left=100,nodeIntegration=true')
+
+    //隐藏聚焦按钮
+    const hideFocusBtn = () => {
+      focusModalBtn.classList.add('disappear')//元素加入class
+      focusModalBtn.classList.remove('smooth-appear')//元素移出class
+      focusModalBtn.removeEventListener('click', clickHandler)
+    }
+  
+    //显示聚焦按钮
+    const showFocusBtn = () => {
+      if (!childWindow) return
+      focusModalBtn.classList.add('smooth-appear')
+      focusModalBtn.classList.remove('disappear')
+      focusModalBtn.addEventListener('click', clickHandler)
+    }
+
+    const clickHandler = () => { 
+      alert(childWindow)
+      childWindow.focus()
+    }
+
+    childWindow.addEventListener('focus', hideFocusBtn)
+    childWindow.addEventListener('blur', showFocusBtn)
+    childWindow.addEventListener('close', () => {
+      hideFocusBtn()
+      childWindow = null
+    })
   })
 
 // const func = async () => {
